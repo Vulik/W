488730d33020a264f3c24725d46f162e7eda437d
Vulik <162407211+Vulik@users.noreply.github.com>
2026-02-28 16:36:51 +0700

    AutoEvo.lua

---
+++ AutoEvo.lua
@@ -0,0 +1,126 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/list/refs/heads/main/game"))()
+
+_G.FishItConfig = _G.FishItConfig or {
+    ["Fishing"] = {
+        ["Auto Perfect"] = false,
+        ["Random Result"] = false,
+
+        ["Auto Favorite"] = true,
+        ["Auto Unfavorite"] = false,
+        ["Fish Name"] = {
+            "Sacred Guardian Squid",
+            {Name = "Ruby", Variant = "Gemstone"},
+        },
+
+        ["Auto Accept Trade"] = true,
+        ["Auto Friend Request"] = false,
+    },
+    ["Auto Trade"] = {
+        ["Enabled"] = true,
+        ["Whitelist Username"] = {"Pugikob"},
+        ["Category Fish"] = {"Secret"},
+        ["Fish Name"] = {
+            {Name = "Ruby", Variant = "Gemstone"},
+        },
+        ["Item Name"] = {
+            "Evolved Enchant Stone",
+        },
+    },
+    ["Farm Coin Only"] = {
+        ["Enabled"] = false,
+        ["Target"] = 0,
+    },
+    ["Selling"] = {
+        ["Auto Sell"] = true,
+        ["Auto Sell Threshold"] = "Mythic",
+        ["Auto Sell Every"] = 50,
+    },
+    ["Doing Quest"] = {
+        ["Auto Ghostfinn Rod"] = true,
+        ["Auto Element Rod"] = false,
+        ["Auto Diamond Rod"] = false,
+        ["Unlock Ancient Ruin"] = true,
+        ["Allowed Sacrifice"] = {},
+        ["FARM_LOC_SECRET_SACRIFICE"] = "",
+
+        ["Minimum Rod"] = "Midnight Rod",
+    },
+    ["WebHook"] = {
+        ["Link Webhook"] = "https://discord.com/api/webhooks/1033311451890327564/Qi0RikL0soMlMYb43B7BmFb2plDB7TQuA9fYJHkDpJIOf7KCNWwk_ENlgFk3fx0a8LcD",
+        ["Auto Sending"] = true,
+        ["Category"] = {"Secret"},
+
+        ["Link Webhook Quest Complete"] = "https://discord.com/api/webhooks/1033311451890327564/Qi0RikL0soMlMYb43B7BmFb2plDB7TQuA9fYJHkDpJIOf7KCNWwk_ENlgFk3fx0a8LcD",
+    },
+    ["Weather"] = {
+        ["Auto Buying"] = true,
+        ["Minimum Rod"] = "Bamboo Rod",
+        ["Weather List"] = {
+            "Wind","Cloudy","Storm"
+        },
+    },
+    ["Potions"] = {
+        ["Auto Use"] = true,
+        ["Minimum Rod"] = "Bamboo Rod",
+    },
+    ["Totems"] = {
+        ["Auto Use"] = true,
+        ["Minimum Rod"] = "Bamboo Rod",
+        ["Buy List"] = {
+            ["Luck Totem"] = 5,
+        },
+    },
+    ["Event"] = {
+        ["Start Farm"] = false,
+        ["Minimum Rod"] = "",
+        ["Event List"] = {},
+    },
+    ["Enchant"] = {
+        ["Auto Enchant"] = true,
+        ["Roll Enchant"] = false,
+        ["Evolved Roll Enchant"] = false,
+        ["Enchant List"] = {
+            "Leprechaun II",
+        },
+        ["Second Enchant"] = false,
+        ["Allowed Sacrifice"] = {},
+        ["Second Enchant List"] = {},
+        ["Minimum Rod"] = "Bamboo Rod",
+    },
+    ["Bait List"] = {
+        ["Auto Buying"] = true,
+        ["Buy List"] = {
+            "Midnight Bait",
+            "Corrupt Bait",
+            "Aether Bait",
+            "Floral Bait",
+            "Singularity Bait"
+        },
+        ["Endgame"] = "Singularity Bait",
+    },
+    ["Rod List"] = {
+        ["Auto Buying"] = true,
+        ["Buy List"] = {
+            "Midnight Rod",
+            "Astral Rod",
+            "Ares Rod",
+            --"Bamboo Rod"
+        },
+        ["Location Rods"] = {
+            ["Ancient Ruin"] = {"Ghostfinn Rod","Bamboo Rod"},
+            ["Tropical Grove"] = {"Starter Rod", "Luck Rod"}
+        },
+        ["Endgame"] = "Bamboo Rod",
+    },
+
+    ["ExtremeFpsBoost"] = false,
+    ["UltimatePerformance"] = false,
+    ["Disable3DRender"] = false,
+    ["AutoRemovePlayer"] = false,
+
+    ["AutoReconnect"] = false,
+    ["HideGUI"] = false,
+    ["EXIT_MAP_IF_DISCONNECT"] = false,
+}
+
+local s,r repeat s,r=pcall(function()return game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/roblog/refs/heads/main/fishit-78c86024ea87c8eca577549807421962.lua")end)wait(1)until s;loadstring(r)()

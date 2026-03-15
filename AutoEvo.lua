a6e6da448ff7408be042d3bceaa95e535da53ad9
Vulik <162407211+Vulik@users.noreply.github.com>
2026-03-15 13:46:33 +0700

    Add 'Luck Totem' to Buy List in AutoEvo.lua

--- AutoEvo.lua
+++ AutoEvo.lua
@@ -75,6 +75,7 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Minimum Rod"] = "Ghostfinn Rod",
         ["Buy List"] = {
             ["Mutation Totem"] = 10,
+            "Luck Totem",
         },
     },
     ["Event"] = {
